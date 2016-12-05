7b2b59fd9efc2bc13000bd38608e504fdfdefc49
alex <tsumarev@gmail.com>
2016-12-05 23:15:32 +0100

    add start

---
+++ typescript/practicalStart/vedningmachine/js/app.js
@@ -0,0 +1,10 @@
+var vendingMachineApp;
+(function (vendingMachineApp) {
+    var VendingMachine = (function () {
+        function VendingMachine() {
+        }
+        return VendingMachine;
+    }());
+    vendingMachineApp.VendingMachine = VendingMachine;
+})(vendingMachineApp || (vendingMachineApp = {}));
+//# sourceMappingURL=app.js.map
---
+++ typescript/practicalStart/vedningmachine/ts/vendingMachine.ts
@@ -0,0 +1,6 @@
+module vendingMachineApp{
+export  class VendingMachine{
+
+}
+
+}
