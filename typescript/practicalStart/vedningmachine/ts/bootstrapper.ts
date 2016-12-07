4fc4a6bddfa280becef1cf7a866d8b099167c4a2
alex cumarev <alex.cumarev@distcotech.com>
2016-12-07 09:14:02 +0100

    add couple more files

--- typescript/practicalStart/vedningmachine/ts/bootstrapper.ts
+++ typescript/practicalStart/vedningmachine/ts/bootstrapper.ts
@@ -1,4 +1,8 @@
 /// <reference path="./vendingMachine"/>
+/// <reference path="typings/knockout.d.ts"/>
+
 import VendingMachine = vendingMachineApp.VendingMachine;
 
 let machine = new VendingMachine();
+
+ko.applyBindings(machine);
