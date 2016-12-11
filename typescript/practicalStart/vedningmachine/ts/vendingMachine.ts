b2344ba7b66f3222caf151dcfd6f36363409d11b
alex <tsumarev@gmail.com>
2016-12-11 13:22:34 +0100

    switch to images

--- typescript/practicalStart/vedningmachine/ts/vendingMachine.ts
+++ typescript/practicalStart/vedningmachine/ts/vendingMachine.ts
@@ -5,6 +5,12 @@
         private paid = ko.observable(0);
         acceptCoin = (coin: Quarter): void => {
           let oldTotal= this.paid()
+
+          if(oldTotal>3){
+            let much=true;
+          }
+          console.log("oldTotal: "+oldTotal);
+          console.log("much: "+much);
           this.paid(oldTotal+coin.Value)
         }
     }
