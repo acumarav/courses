4208746d0aaeffed2b1031a7248a7e64992630fe
alex <tsumarev@gmail.com>
2017-02-08 22:59:43 +0100

    ang2 getting started

--- angular2gettingstarted/apm/app/app.component.ts
+++ angular2gettingstarted/apm/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+/*
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>{{pageTitle}}</h1>
+        <div>My first component</div>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = "Angular2: Getting Started!";
+}
+*/
+
+import {Component} from '@angular/core';
+@Component({
+    selector: 'pm-app',
+    template:`
+<div><h1>{{pageTitle}}</h1>
+<div>My first component</div>
+</div>
+`
+})
+export class AppComponent{
+    pageTitle:string = "Acme Product Manager";
+}
