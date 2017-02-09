96ef4c73513f63894dbcbd0441a80bffd4dd7c67
alex cumarev <alex.cumarev@distcotech.com>
2017-02-09 09:39:36 +0100

    starting angular 2 - add product list component

--- angular2gettingstarted/apm/app/app.component.ts
+++ angular2gettingstarted/apm/app/app.component.ts
@@ -1,24 +1,10 @@
-/*
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'pm-app',
-    template: `
-        <h1>{{pageTitle}}</h1>
-        <div>My first component</div>
-    `
-})
-export class AppComponent {
-    pageTitle: string = "Angular2: Getting Started!";
-}
-*/
-
 import {Component} from '@angular/core';
 @Component({
     selector: 'pm-app',
     template:`
 <div><h1>{{pageTitle}}</h1>
 <div>My first component</div>
+<pm-products></pm-products>
 </div>
 `
 })
