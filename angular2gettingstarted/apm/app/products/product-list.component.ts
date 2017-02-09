39a976b84644db8bfbff869d6e6671b2d189d79c
alex <tsumarev@gmail.com>
2017-02-09 23:14:59 +0100

    ang2 getting started - filter product

--- angular2gettingstarted/apm/app/products/product-list.component.ts
+++ angular2gettingstarted/apm/app/products/product-list.component.ts
@@ -1,26 +1,29 @@
-import {Component} from '@angular/core'
+import {Component,OnInit} from '@angular/core'
+import {IProduct} from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls:['app/products/product-list.component.css']
 })
-export class ProductListComponent{
+
+export class ProductListComponent implements OnInit{
 
     pageTitle: string="Product List!";
 
     imageWidth: number= 50;
     imageMargin: number =2;
 
-    products: any[] =[
+    products: IProduct[] =[
         {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+            productId: 4,
+            productName: "Leaf Rake",
+            productCode: "GDN-0011",
+            releaseDate: "March 19, 2016",
+            description: "Leaf rake with 48-inch wooden handle.",
+            price: 19.95,
+            starRating: 3.2,
+            imageUrl: "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         },
         {
             "productId": 2,
@@ -64,9 +67,13 @@ export class ProductListComponent{
         }
     ];
 
+    ngOnInit():void{
+        console.log('On  Init');
+    }
+
     showImage: boolean =false;
 
-    listFilter: string ='cart';
+    listFilter: string ='';
 
     toggleImage(): void{
         this.showImage=!this.showImage;
