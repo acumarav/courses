e6a3629fbe8ce83a8fd8368ffa4d82f530fdc7c2
alex <tsumarev@gmail.com>
2017-03-15 22:04:14 +0100

    part 04

---
+++ js/reasoning_about_asynchronous_javascript/js/part04.js
@@ -0,0 +1,6 @@
+//document.getElementById('content').innerHTML = 'Main content here from JS';
+
+
+setTimeout(function () {
+    document.getElementById('content').innerHTML = 'Main content here from JS';
+},100);
