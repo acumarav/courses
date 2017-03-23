d4f033941f540d9b83498e11830568206ec191fc
alex cumarev <alex.cumarev@distcotech.com>
2017-03-23 10:26:30 +0100

    one more class

--- kotlin/securitytools/src/main/kotlin/com/rsk/Person.kt
+++ kotlin/securitytools/src/main/kotlin/com/rsk/Person.kt
@@ -6,7 +6,14 @@ interface Signatory {
 }
 
 open class Person(val name: String, var age: Int) : Signatory {
+    var isMarried = false
+
+    constructor(name: String, age: Int, isMarried: Boolean) : this(name, age) {
+        this.isMarried = isMarried
+    }
+
     override fun sign() = println("I am $name, and I can sign documents")
+
     override fun toString(): String {
         return "$name, $age years old"
     }
