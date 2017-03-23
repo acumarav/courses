cbc2f3e933cfba0f201b02fcb33d0689ca4449c7
alex cumarev <alex.cumarev@distcotech.com>
2017-03-23 10:15:16 +0100

    one more class

---
+++ kotlin/securitytools/src/main/kotlin/com/rsk/Person.kt
@@ -0,0 +1,15 @@
+package com.rsk
+
+
+interface Signatory {
+    fun sign()
+}
+
+class Person : Signatory {
+    override fun sign() = println("I can sign documents")
+}
+
+fun main(args: Array<String>){
+    val p=Person()
+    p.sign()
+}
