0a31cb2da15ccb0eb6a01f339a1ad4d402efa317
alex <tsumarev@gmail.com>
2017-03-25 18:27:19 +0100

    app fireframe

--- kotlin/securitytools/src/main/kotlin/Program.kt
+++ kotlin/securitytools/src/main/kotlin/Program.kt
@@ -2,12 +2,21 @@ import com.rsk.Providers
 
 fun main(args: Array<String>) {
 
-    val allProviders=Providers.getProviders();
+    val providers = Providers()
+
+    val details = providers.getAllProviders()
+    details.forEach (::println)
+
+}
+
+private fun getAllProviders(fn: (String, String) -> Unit) {
+    val allProviders = Providers.getProviders();
 
     val it = allProviders.iterator()
     while (it.hasNext()) {
         val provider = it.next()
         println(provider.name)
-        provider.forEach { key, value -> println("\t$key: $value") }
+
+        provider.forEach { key, value -> fn.invoke(key.toString(), value.toString()) }
     }
 }
--- kotlin/securitytools/src/main/kotlin/com/rsk/Providers.kt
+++ kotlin/securitytools/src/main/kotlin/com/rsk/Providers.kt
@@ -3,17 +3,32 @@ package com.rsk
 import java.security.Provider
 import java.security.Security
 
+data class ProviderDetails(val providerName: String, val name: String)
+
 class Providers {
-    fun getProviders(): List<Provider>{
+    fun getProviders(): List<Provider> {
         val providers = Security.getProviders()
-        var lisfOfProviders:List<Provider> = providers.asList()
+        var lisfOfProviders: List<Provider> = providers.asList()
         return lisfOfProviders
     }
 
-    companion object{
-        fun getProviders(): List<Provider>{
+    fun getAllProviders(): List<ProviderDetails> {
+        val providers = Security.getProviders()
+        val listOfProviders = mutableListOf<ProviderDetails>()
+
+        providers.forEach {
+            provider ->
+            val providerDetails = provider.entries.map { ProviderDetails(provider.name, it.key.toString()) }
+            listOfProviders += providerDetails
+        }
+        return listOfProviders
+
+    }
+
+    companion object {
+        fun getProviders(): List<Provider> {
             val providers = Security.getProviders()
-            var lisfOfProviders:List<Provider> = providers.asList()
+            var lisfOfProviders: List<Provider> = providers.asList()
             return lisfOfProviders
         }
     }
