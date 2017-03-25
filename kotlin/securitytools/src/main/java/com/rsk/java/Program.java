03f2baed99d2e1b5d2e0e8f43eb69191945202ea
alex <tsumarev@gmail.com>
2017-03-25 19:11:16 +0100

    app fireframe

---
+++ kotlin/securitytools/src/main/java/com/rsk/java/Program.java
@@ -0,0 +1,15 @@
+package com.rsk.java;
+
+import com.rsk.kotlin.Meeting;
+
+/**
+ * Created by alex on 3/25/2017.
+ */
+public class Program {
+    public static void main(String[] args){
+        Meeting board=new Meeting("Board Meeting");
+        board.setLocation("London");
+
+        System.out.println("Meeting is in: "+board.getLocation());
+    }
+}
---
+++ kotlin/securitytools/src/main/kotlin/com/rsk/kotlin/Meeting.kt
@@ -0,0 +1,8 @@
+package com.rsk.kotlin
+
+/**
+ * Created by alex on 3/25/2017.
+ */
+class Meeting(val title: String) {
+    var location =""
+}
