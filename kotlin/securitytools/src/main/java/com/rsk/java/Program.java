ede6bbeadbc1c4f0bc3ce8ce7a3e6a6ceae93a6d
alex <tsumarev@gmail.com>
2017-03-25 19:20:28 +0100

    app fireframe

--- kotlin/securitytools/src/main/java/com/rsk/java/Program.java
+++ kotlin/securitytools/src/main/java/com/rsk/java/Program.java
@@ -6,10 +6,12 @@ import com.rsk.kotlin.Meeting;
  * Created by alex on 3/25/2017.
  */
 public class Program {
-    public static void main(String[] args){
-        Meeting board=new Meeting("Board Meeting");
+    public static void main(String[] args) {
+        Meeting board = new Meeting("Board Meeting");
         board.setLocation("London");
 
-        System.out.println("Meeting is in: "+board.getLocation());
+        System.out.println("Meeting is in: " + board.getLocation());
+
+        board.description = "A board meeting";
     }
 }
--- kotlin/securitytools/src/main/kotlin/com/rsk/kotlin/Meeting.kt
+++ kotlin/securitytools/src/main/kotlin/com/rsk/kotlin/Meeting.kt
@@ -5,4 +5,6 @@ package com.rsk.kotlin
  */
 class Meeting(val title: String) {
     var location =""
+    @JvmField
+    var description =""
 }
