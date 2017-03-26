2728d30c2c61820c1fee67fc5d64fdc4e927c55d
alex <tsumarev@gmail.com>
2017-03-26 11:41:53 +0200

    kotlin tests

--- kotlin/securitytools/src/main/kotlin/com/rsk/Calculator.kt
+++ kotlin/securitytools/src/main/kotlin/com/rsk/Calculator.kt
@@ -1,7 +1,15 @@
 package com.rsk
 
+import kotlin.coroutines.experimental.buildIterator
+
 class Calculator(val output: Result) {
+    var total = 0;
+
     fun add(x: Int, y: Int): Int = x + y
+
+    fun accumulate(x: Int): Unit {
+        total += x
+    }
 }
 
 interface Result {
--- kotlin/securitytools/src/test/kotlin/com/rsk/CalculatorSpek.kt
+++ kotlin/securitytools/src/test/kotlin/com/rsk/CalculatorSpek.kt
@@ -2,13 +2,30 @@ package com.rsk;
 
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 import org.junit.jupiter.api.Assertions
 
-public class CalculatorSpek : Spek({
-    val calculator = Calculator(NullResult())
-    describe("the calculator"){
-        val result= calculator.add(12,13)
+class CalculatorSpek : Spek({
+    var calculator: Calculator? = null
+    describe("the calculator") {
+        beforeEachTest {
+            calculator = Calculator(NullResult())
+        }
 
-        Assertions.assertEquals(25,result)
+        it("should add two number") {
+            val result = calculator?.add(12, 13)
+            Assertions.assertEquals(25, result)
+        }
+
+        it("should accumulate  one number") {
+            calculator?.accumulate(47)
+            Assertions.assertEquals(47, calculator?.total)
+        }
+
+        it("should accumulate  two number") {
+            calculator?.accumulate(4)
+            calculator?.accumulate(7)
+            Assertions.assertEquals(11, calculator?.total)
+        }
     }
 })
