1dbc928228f0110bd17b8bce7b84ef40f28eec70
alex <tsumarev@gmail.com>
2017-04-15 21:25:46 +0200

    obfuscation issue - add build script

--- java/spring_boot_obfuscation/build.sh
+++ java/spring_boot_obfuscation/build.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 srcpath=`pwd`
+profile=''
+#-P obfuscation
+
+count=1
+for param in "$@"
+do
+#echo "\$@ Parameter #$count = $param"
+count=$(( $count + 1 ))
+if [ "$param" == "-P" ] 
+then	
+	nextparam=${!count}	
+	profile="-P $nextparam"
+	echo "building with maven Profile: $profile"
+fi
+done
 
 while [ -n "$1" ]
 do
 case "$1" in
 -b) echo "building bsmod"	
 	pushd $srcpath/bsmod
-	mvn -T 1C clean install -Dmaven.test.skip=true -P obfuscation	
+	mvn -T 1C clean install -Dmaven.test.skip=true $profile
 	popd ;;
 -u)	echo "building uxmod"
 	pushd $srcpath/uxmod	 
-	mvn clean install -Dmaven.test.skip=true -P obfuscation
+	mvn clean install -Dmaven.test.skip=true $profile
 	popd ;;
 esac
 shift
