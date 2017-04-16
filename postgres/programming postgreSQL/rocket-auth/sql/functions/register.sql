791bd50d24848d56c7f00ba24e52804d27ce760e
alex <tsumarev@gmail.com>
2017-04-16 19:05:42 +0200

    add register function

---
+++ postgres/programming postgreSQL/rocket-auth/sql/functions/register.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION register(new_email VARCHAR, password VARCHAR)
+  RETURNS TABLE(
+  new_id BIGINT,
+  validation_token VARCHAR (36),
+  success BOOLEAN,
+  MESSAGE VARCHAR(255)
+) AS $$
+DECLARE
+BEGIN
+  set search_path=membership;
+  if not exists
+    (select users.email from users where users.email = new_email)
+    then
+    insert into users(email, hashed_password)
+    values(new_email, crypt(password, gen_salt('bf',10)))
+    returning id into new_id;
+
+    validation_token := random_string(36);
+    success := true;
+    message := 'Welcome!';
+    else
+    select 'This email is already registered' into message;
+  end if;
+    return query
+    select new_id, validation_token, success, message;
+    END;
+$$ LANGUAGE plpgsql;
