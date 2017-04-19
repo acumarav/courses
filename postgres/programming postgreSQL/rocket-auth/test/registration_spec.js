bead67c5ac2e2235517746ebc75e6b3c622a2085
alex <tsumarev@gmail.com>
2017-04-19 22:35:00 +0200

    register not working draft

---
+++ postgres/programming postgreSQL/rocket-auth/test/registration_spec.js
@@ -0,0 +1,34 @@
+var assert = require("assert");
+
+var Helpers = require("./helpers");
+var helpers = new Helpers();
+
+
+describe('Registration', function () {
+    var db = null;
+    before(function (done) {
+            helpers.initDb(function (err, res) {
+                db = res;
+                console.log("DB Init done");
+                done();
+            });
+        }
+    );
+
+    describe("with valid creds", function () {
+        var regResult = null;
+        before(function (done) {
+            db.membership.register(['test1@test.com', 'password'], function (err, res) {
+                assert(err === null, err);
+                regResult = res[0];
+                done();
+            });
+        });
+        it("is successful", function () {
+            assert(regResult.success);
+        });
+        it('returns a new id', function () {
+            assert(regResult.new_id);
+        });
+    });
+});
