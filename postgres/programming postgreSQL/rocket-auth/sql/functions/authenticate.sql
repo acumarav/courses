193da47833222bad8fbf74b9fd6854ecc1cbeb4a
alex <tsumarev@gmail.com>
2017-04-23 17:55:40 +0200

    authenticate working

--- postgres/programming postgreSQL/rocket-auth/sql/functions/authenticate.sql
+++ postgres/programming postgreSQL/rocket-auth/sql/functions/authenticate.sql
@@ -11,6 +11,7 @@ DECLARE
   return_message VARCHAR(50);
   success        BOOLEAN := false;
   found_id       BIGINT;
+  can_login BOOLEAN := FALSE;
 BEGIN
   SET SEARCH_PATH = membership;
   if(prov = 'local') then
@@ -28,24 +29,33 @@ BEGIN
 
   IF (found_id IS NOT NULL)
   THEN
-    SELECT * FROM users
-    WHERE users.id = found_id
-    INTO found_user;
-    INSERT INTO logs (user_id, subject, entry)
-    VALUES (found_id, 'Authentication', 'Logged user in using ' || prov);
-    --set last login
-    UPDATE users
-    SET last_login = now(), login_count = login_count + 1
-    WHERE users.id = found_id;
-    --set the display name
-    IF (found_user.first IS NOT NULL)
-    THEN
-      display_name := concat(found_user.first, ' ', found_user.last);
-    ELSE
-      display_name := found_user.email;
-    END IF;
-    success := TRUE;
-    return_message := 'Welcome! '|| display_name;
+    SELECT * FROM users WHERE users.id = found_id INTO found_user;
+    SELECT status.can_login from status where id=found_user.status_id into can_login;
+
+    if(can_login) then
+      INSERT INTO logs (user_id, subject, entry)
+      VALUES (found_id, 'Authentication', 'Logged user in using ' || prov);
+      --set last login
+      UPDATE users
+      SET last_login = now(), login_count = login_count + 1
+      WHERE users.id = found_id;
+      --set the display name
+      IF (found_user.first IS NOT NULL)
+      THEN
+        display_name := concat(found_user.first, ' ', found_user.last);
+      ELSE
+        display_name := found_user.email;
+      END IF;
+      success := TRUE;
+      return_message := 'Welcome! '|| display_name;
+      ELSE
+        --log failed attempt
+        insert into logs(user_id, subject, entry)
+        values(found_id, 'Authentication', 'User tried to login, is locked out');
+
+        success := false;
+        return_message := 'This user is currently locked out';
+      END IF;
   ELSE
     success := FALSE;
     return_message := 'Invalid login credentials';
@@ -54,22 +64,19 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+create or replace function authenticate_by_token(token varchar)
+  returns table(
+    return_id bigint,
+    email varchar(255),
+    display_name varchar(50),
+    success boolean,
+    message varchar(50)
+  ) as $$
+begin
+  return query
+  select * from authenticate('token',token,'token');
+end;
+$$
+language plpgsql;
 
---CREATE OR REPLACE FUNCTION authenticate(em VARCHAR, pass VARCHAR)
---  RETURNS TABLE(
---    return_id           BIGINT,
---    email        VARCHAR(255),
---    display_name VARCHAR(50),
---    success      BOOLEAN,
---    message      VARCHAR(50)
---  ) AS $$
---DECLARE
---  hashed_pw VARCHAR(128);
---BEGIN
---  SET SEARCH_PATH = membership;
---  hashed_pw = crypt(pass, gen_salt('bf', 10));
---  RETURN QUERY
---  SELECT *
---  FROM authenticate(em, hashed_pw, 'local');
---END;
---$$ LANGUAGE plpgsql;
+
