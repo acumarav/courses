dcfc8f73920a8f58fb14124c2349a3daa130c8be
alex <tsumarev@gmail.com>
2017-04-23 19:44:13 +0200

    change status working

--- postgres/programming postgreSQL/rocket-auth/sql/functions/change_password.sql
+++ postgres/programming postgreSQL/rocket-auth/sql/functions/change_password.sql
@@ -5,5 +5,20 @@ create or REPLACE  FUNCTION change_password(em VARCHAR, old_pass VARCHAR, new_pa
     found_id BIGINT;
   BEGIN
     --found user by id
+    select locate_user_by_password(em, old_pass) into found_id;
+    if found_id is not null then
+      update logins set provider_token = crypt(new_pass, gen_salt('bf',10))
+      where user_id = found_id and provider = 'local';
+
+      insert into logs(user_id, subject, entry)
+        VALUES (found_id,'Authentication','Password changed');
+
+      --add note
+      insert into notes(user_id, note)
+          values(found_id,'Successfully changed password');
+      else
+
+    END IF;
+    return get_user(em);
   END;
   $$ LANGUAGE plpgsql;
