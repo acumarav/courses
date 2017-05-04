f18fe15913227573219f54c4d530f81afeec36b5
alex cumarev <alex.cumarev@distcotech.com>
2017-05-04 09:25:24 +0200

    set up spring boot

--- java/spring_boot_obfuscation/uxmod/src/main/java/org/alext/learning/UxApp.java
+++ java/spring_boot_obfuscation/uxmod/src/main/java/org/alext/learning/UxApp.java
@@ -1,15 +1,40 @@
 package org.alext.learning;
 
+import org.alext.learning.model.User;
+import org.alext.learning.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class UxApp {
     public static void main(String[] args) {
         SpringApplication.run(UxApp.class, args);
+    }
+
+    class Initializer implements CommandLineRunner{
+
+        private final UserRepository repo;
+
+        @Autowired
+        Initializer(UserRepository repo) {
+            this.repo = repo;
+
+            Stream.of("Alex","Dmitry","Jon","Paul","Maria","Eva").forEach(name->{
+                User user=new User();
+                user.setName(name);
+                user.setEmail(name+"@dummy.com");
+                repo.save(user);
+            });
+
+        }
+
+        @Override
+        public void run(String... strings) throws Exception {
+
+        }
     }
 }
