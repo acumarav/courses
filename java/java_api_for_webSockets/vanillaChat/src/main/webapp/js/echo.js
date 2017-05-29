5a4e4578fd5c463a31d39ce47d323ccd1ace8df7
alex cumarev <alex.cumarev@distcotech.com>
2017-05-29 09:36:51 +0200

    working chat

--- java/java_api_for_webSockets/vanillaChat/src/main/webapp/js/echo.js
+++ java/java_api_for_webSockets/vanillaChat/src/main/webapp/js/echo.js
@@ -1,21 +1,61 @@
 "use strict";
 
-alert("Go!");
+//alert("Go!");
 
 var Echo = Echo || {};
 
-Echo.initialize = function () {
-  var ep = '/websocket/echo';
+Echo.connect = function (host) {
+  console.log("connection to: " + host);
 
-  if(window.location.protocol == 'http'){
-
+  if ('WebSocket' in window) {
+    Echo.socket = new WebSocket(host);
+  }
+  else if ('MozWebSocket' in window) {
+    Echo.socket = new MozWebSocket(host);
   }
   else {
-
+    console.error('web socket is not supported by your browser!');
+    return;
   }
+
+  Echo.socket.onopen = function () {
+    console.log("Info: connection opened");
+    $("#chatEditor").keydown(function (evt) {
+      if (evt.keyCode == 13) {
+        Echo.sendMessage();
+      }
+    })
+  }
+
+  Echo.socket.onmessage = function (message) {
+    console.log("message: " + message.data);
+    $("#chatLog").append(message.data);
+  }
+
+  Echo.socket.onclose = function () {
+    console.log("Info: connection has been closed");
+  }
+}
+
+Echo.sendMessage = function () {
+  let msg = $("#chatEditor").val();
+  $("#chatEditor").val('');
+  if (msg && msg != '') {
+    Echo.socket.send(msg);
+  }
+}
+
+Echo.initialize = function () {
+  var ep = '/websocket/echo';
+  let url = `${window.location.host}${ep}`;
+  if (window.location.protocol == 'http:') {
+    url = 'ws://' + url;
+  }
+  else {
+    url = 'wss://' + url;
+  }
+  Echo.connect(url);
 };
 
-
-
-$("chatEditor").text("hello world");
+Echo.initialize();
 
