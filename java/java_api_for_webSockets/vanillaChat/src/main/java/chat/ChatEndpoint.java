c024b03d2ff0a0b813cd4f592fed9530e4be7247
alex cumarev <alex.cumarev@distcotech.com>
2017-05-30 15:53:59 +0200

    new chat

---
+++ java/java_api_for_webSockets/vanillaChat/src/main/java/chat/ChatEndpoint.java
@@ -0,0 +1,49 @@
+package chat;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@ServerEndpoint("/websocket/chat")
+//public class ChatEndpoint implements MessageHandler.Whole<String>{
+public class ChatEndpoint {
+
+    private Session session;
+    static List<ChatEndpoint> clients = new CopyOnWriteArrayList<ChatEndpoint>();
+
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config) {
+        this.session = session;
+        clients.add(this);
+
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        System.out.println("socket closed: "+reason.getReasonPhrase());
+        clients.remove(this);
+    }
+
+    @OnMessage
+    public void onMessage(String message){
+        broadcast("bc"+message);
+    }
+
+    private void broadcast(String message) {
+        for(ChatEndpoint client : clients){
+            try {
+                client.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                clients.remove(this);
+                try {
+                    client.session.close();
+                } catch (IOException e1) {
+                    //do nothing
+                }
+
+            }
+        }
+    }
+}
