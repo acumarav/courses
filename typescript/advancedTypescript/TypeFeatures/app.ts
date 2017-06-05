8ea0090bbd8fb7f43f4ccd5133cfc7d9ffac058d
Alex Cumarev <tsumarev@gmail.com>
2017-06-05 23:21:56 +0200

    ch 01 code

--- typescript/advancedTypescript/TypeFeatures/app.ts
+++ typescript/advancedTypescript/TypeFeatures/app.ts
@@ -16,3 +16,44 @@ console.log("APP TS is running...")
 LogFavoriteBooks(util.GetAllBooks());
 
 
+let [book1, book2] = util.GetAllBooks();
+let {title: booktitle, author: bookauthor} = book1;
+console.log(booktitle);
+console.log(bookauthor);
+//spread operation
+
+let poets: string[] = ['Shelley', 'Collins', 'Hughes'];
+let authors: string[] = ['Bykov', 'Kupala', ...poets];
+console.log(authors);
+
+
+//Typles
+let myTyple: [number, string] = [10, "Alex"]
+console.log(myTyple);
+
+//Key-value
+
+let catalogLocation: [string, Book] = ["A 123.456", book1];
+
+interface  KeyValuePair<K, V> extends Array<K | V> {
+    0: K;
+    1: V;
+}
+
+let anotherLocation: KeyValuePair<string, Book> = ["A 123.456", book2];
+//anotherLocation[2]=book2;
+
+//union and intersection types
+
+let allBooks: Book[] = util.GetAllBooks();
+let allMagazines: Magazine[] = util.GetAllMagazines();
+let readingMaterial: Book | Magazine = allMagazines[0]; //allBooks[0];
+
+
+function PrintTitle(item: Book | Magazine) {
+    console.log(item.title);
+}
+
+//let serialNovel: Book & Magazine = allBooks[0];
+
+
