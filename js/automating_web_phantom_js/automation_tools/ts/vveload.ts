e4baf69f0cd50b62d5354c8c3c3df68ea042dfcc
alex cumarev <alex.cumarev@distcotech.com>
2017-06-05 16:52:48 +0200

    switch to typescript - not working

---
+++ js/automating_web_phantom_js/automation_tools/ts/vveload.ts
@@ -0,0 +1,42 @@
+import {Casper, CasperOptions} from "casperjs";
+
+let options: CasperOptions = {}
+
+options.viewportSize = {width: 1280, height: 1024};
+
+
+let casper: Casper = new Casper(options);
+
+casper.start('http://vveplay.com').then(() => {
+    //let title:String =  this.evaluate(()=>this.getTitle())
+    let title: String = this.getTitle();
+    this.capture("./output/img.png")
+
+});
+
+casper.run(() => {
+    console.log("run Complete");
+}, 3000);
+
+/*
+ casper.start('http://vveplay.com', function () {
+ //this.capture('./output/img.png');
+
+ var title = this.evaluate(function () {
+ return this.getTitle();
+ });
+
+ var casper = this;
+
+ this.waitForSelector('.gallery .video', function () {
+ var videos = document.querySelector('.gallery .video');
+ casper.capture('./output/img.png');
+ console.log(videos);
+ }, 5000);
+ console.log('Title: ' + title);
+ //this.capture('./output/img.png');
+
+ });
+ casper.run();*/
+
+
---
+++ js/automating_web_phantom_js/automation_tools/typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="modules/casperjs/index.d.ts" />
