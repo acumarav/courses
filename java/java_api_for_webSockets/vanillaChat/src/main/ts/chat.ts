3ebb67f22be893f7db47d69b16c54e6180436add
alex cumarev <alex.cumarev@distcotech.com>
2017-06-06 09:24:00 +0200

    new chat

--- java/java_api_for_webSockets/vanillaChat/src/main/ts/chat.ts
+++ java/java_api_for_webSockets/vanillaChat/src/main/ts/chat.ts
@@ -1,4 +1,22 @@
-
+/// <reference path="typings/jquery.d.ts" />
 
 class Chat {
+
+    constructor(private chatWSEndpoint: string) {
+        console.log("WS URL: " + chatWSEndpoint);
+        $("#join").click(this.joinChat);
+    }
+
+    private joinChat = () => {
+        console.log("JOin Chat!");
+    }
+
 }
+
+console.log("starting....");
+const wsUrl = `${window.location.host}/websocket/chat`;
+let app = new Chat(wsUrl);
+
+
+
+
